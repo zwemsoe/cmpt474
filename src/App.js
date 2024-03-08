db880f8a23b515c24e68e01c46e08edf8e2a8e07
jay <zwemsoe@gmail.com>
2024-03-07 20:22:43 -0800

    auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>CMPT474</p>
-      </header>
-    </div>
+    <View className='App'>
+      <Card>
+        <Image src={logo} className='App-logo' alt='logo' />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Amplify } from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
